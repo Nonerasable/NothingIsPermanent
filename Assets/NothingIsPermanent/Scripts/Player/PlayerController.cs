cfc8087db38e9c4550a61e787d62dfb280075d30
noxymu5 <mac41423@gmail.com>
2025-06-28 17:01:10 +0400

    added player prefab, global managers prefab

---
+++ Assets/NothingIsPermanent/Scripts/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _walkingSpeed = 7.5f;
+    [SerializeField] private float _runningSpeed = 11.5f;
+    [SerializeField] private float _jumpSpeed = 8.0f;
+    [SerializeField] private float _gravity = 20.0f;
+    [SerializeField] private Camera _playerCamera;
+    [SerializeField] private float _lookSpeed = 2.0f;
+    [SerializeField] private float _lookXLimit = 45.0f;
+    
+    private CharacterController _characterController;
+    private Vector3 _moveDirection = Vector3.zero;
+    private float _rotationX = 0;
+    private InputSystem_Actions.PlayerActions _actions;
+
+    void Start() {
+        _actions = InputActionsProvider.Inst.Actions.Player;
+        _characterController = GetComponent<CharacterController>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        // Press Left Shift to run
+        bool isRunning = _actions.Sprint.IsPressed();
+        Vector2 moveInput = _actions.Move.ReadValue<Vector2>();
+        
+        float curSpeedX = (isRunning ? _runningSpeed : _walkingSpeed) * moveInput.y;
+        float curSpeedY = (isRunning ? _runningSpeed : _walkingSpeed) * moveInput.x;
+        float movementDirectionY = _moveDirection.y;
+        _moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (_actions.Jump.IsPressed() && _characterController.isGrounded)
+        {
+            _moveDirection.y = _jumpSpeed;
+        }
+        else
+        {
+            _moveDirection.y = movementDirectionY;
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (!_characterController.isGrounded)
+        {
+            _moveDirection.y -= _gravity * Time.deltaTime;
+        }
+
+        // Move the controller
+        _characterController.Move(_moveDirection * Time.deltaTime);
+
+        Vector2 lookInput = _actions.Look.ReadValue<Vector2>();
+        
+        _rotationX += -lookInput.y * _lookSpeed;
+        _rotationX = Mathf.Clamp(_rotationX, -_lookXLimit, _lookXLimit);
+        _playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, lookInput.x * _lookSpeed, 0);
+    }
+}
