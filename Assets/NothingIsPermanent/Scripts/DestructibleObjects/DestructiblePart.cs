e0c0659c3b13f18e129f2619480cd24f0841b08b
noxymu5 <mac41423@gmail.com>
2025-06-28 21:03:32 +0400

    make destruction by sphere

--- Assets/NothingIsPermanent/Scripts/DestructibleObjects/DestructiblePart.cs
+++ Assets/NothingIsPermanent/Scripts/DestructibleObjects/DestructiblePart.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -7,20 +8,28 @@ public class DestructiblePart : MonoBehaviour {
     
     [SerializeField] private DestructibleMaterialType _materialType;
     [SerializeField] private DestructiblePart _attachedTo;
-    [SerializeField] [Min(0.1f)] private float _health = 10f;
     [SerializeField] [Min(0.1f)] private float _destroySpeedPerSecond = 1;
 
+    private MeshFilter _meshFilter;
+    
     private bool _isBeingDestroyed = false;
+    private float _currentDestructionRadius = 0;
+    private Vector3 _destructionStartPointLcs;
+    
+    private List<DestructiblePart> _attachedParts = new();
     private FixedJoint _joint;
 
-    [ContextMenu("Start")]
-    private void StartDestroy() {
+    public void RegisterAttachedPart(DestructiblePart part) {
+        _attachedParts.Add(part);
+    }
+
+    public void StartDestruction(Vector3 destructionStartPointWcs) {
+        _destructionStartPointLcs = transform.InverseTransformPoint(destructionStartPointWcs);
         _isBeingDestroyed = true;
     }
-    
-    [ContextMenu("Stop")]
-    private void StopDestroy() {
-        _isBeingDestroyed = false;        
+
+    private void Awake() {
+        _meshFilter = GetComponent<MeshFilter>();
     }
 
     private void Start() {
@@ -31,12 +40,15 @@ public class DestructiblePart : MonoBehaviour {
         _attachedTo.OnBeforeDestroy += HandleParentObjectDestroyed;
         _joint = gameObject.AddComponent<FixedJoint>();
         _joint.connectedBody = _attachedTo.GetComponent<Rigidbody>();
+        _attachedTo.RegisterAttachedPart(this);
     }
 
     private void OnDestroy() {
         if (_attachedTo) {
             _attachedTo.OnBeforeDestroy -= HandleParentObjectDestroyed;
         }
+        
+        _attachedParts.Clear();
     }
 
     private void Update() {
@@ -44,10 +56,70 @@ public class DestructiblePart : MonoBehaviour {
             return;
         }
 
-        _health -= Time.deltaTime * _destroySpeedPerSecond;
-        if (_health <= 0) {
-            DestroyPart();
+        _currentDestructionRadius += Time.deltaTime * _destroySpeedPerSecond;
+        if (!AreBoundsInsideDestructionSphere()) {
+            return;
         }
+
+        DestroyPart();
+    }
+
+    private void OnDrawGizmos() {
+        if (!_meshFilter) {
+            return;
+        }
+        
+        Vector3 startPointWcs = transform.TransformPoint(_destructionStartPointLcs);
+        
+        foreach (Vector3 vertex in GetMeshBoundingVertices()) {
+            if (Vector3.Distance(vertex, startPointWcs) > _currentDestructionRadius) {
+                Gizmos.color = Color.red;
+            }
+            else {
+                Gizmos.color = Color.green;
+            }
+            Gizmos.DrawSphere(vertex, 0.05f);            
+        }
+
+        if (_isBeingDestroyed) {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(startPointWcs, 0.05f);
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(startPointWcs, _currentDestructionRadius);   
+        }
+    }
+
+    private bool AreBoundsInsideDestructionSphere() {
+        Vector3 startPointWcs = transform.TransformPoint(_destructionStartPointLcs);
+        foreach (Vector3 vertex in GetMeshBoundingVertices()) {
+            if (Vector3.Distance(vertex, startPointWcs) > _currentDestructionRadius) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private List<Vector3> GetMeshBoundingVertices() {
+        Bounds bounds = _meshFilter.mesh.bounds;
+        Vector3 center = bounds.center;
+        Vector3 extents = bounds.extents;
+
+        List<Vector3> boundVertices = new() {
+            //lower 
+            transform.TransformPoint(new Vector3(center.x + extents.x, center.y - extents.y, center.z + extents.z)),
+            transform.TransformPoint(new Vector3(center.x + extents.x, center.y - extents.y, center.z - extents.z)),
+            transform.TransformPoint(new Vector3(center.x - extents.x, center.y - extents.y, center.z + extents.z)),
+            transform.TransformPoint(new Vector3(center.x - extents.x, center.y - extents.y, center.z - extents.z)),
+            //upper
+            transform.TransformPoint(new Vector3(center.x + extents.x, center.y + extents.y, center.z + extents.z)),
+            transform.TransformPoint(new Vector3(center.x + extents.x, center.y + extents.y, center.z - extents.z)),
+            transform.TransformPoint(new Vector3(center.x - extents.x, center.y + extents.y, center.z + extents.z)),
+            transform.TransformPoint(new Vector3(center.x - extents.x, center.y + extents.y, center.z - extents.z))
+        };
+
+        return boundVertices;
     }
 
     private void HandleParentObjectDestroyed() {
